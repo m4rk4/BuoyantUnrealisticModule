d7c336645a2cfb69737a3f0541014bcbc2481215
m4rk4 <m4rk4@outlook.com>
2021-08-27 16:41:44 -0400

    Update Bandcamp

--- feedhandlers/bandcamp.py
+++ feedhandlers/bandcamp.py
@@ -55,7 +55,7 @@ def get_content(url, args, save_debug=False):
         item['author']['name'] = audio_json['byArtist']['name']
         item['tags'] = audio_json['keywords'].copy()
         item['_image'] = audio_json['image']
-        poster = '{}/image?width=128&url='.format(config.server, quote_plus(audio_json['image']))
+        poster = '{}/image?height=128&url={}&overlay=audio'.format(config.server, quote_plus(audio_json['image']))
 
         if audio_json.get('description'):
           item['summary'] = audio_json['description']
@@ -68,27 +68,36 @@ def get_content(url, args, save_debug=False):
               if 'mp3' in prop['name']:
                 item['_audio'] = prop['value']
                 break
-          audio_url = '{}/audio?url='.format(config.server, quote_plus(audio_json['@id']))
-          item['content_html'] = '<center><table style="width:480px;"><tr><td width="30%" rowspan="3"><img width="100%" src="{}"></td><td><a href="{}"><b>{}</b></a></td></tr><tr><td><small>'.format(poster, audio_json['@id'], audio_json['name'])
+          audio_src = '{}/audio?url={}'.format(config.server, quote_plus(audio_json['@id']))
+          desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4><small>'.format(audio_json['@id'], audio_json['name'])
           if audio_json.get('inAlbum'):
-            item['content_html'] += 'from <a href="{}">{}</a><br />'.format(audio_json['inAlbum']['@id'], audio_json['inAlbum']['name'])
-          item['content_html'] += 'by <a href="{}">{}</a></small></td></tr>'.format(audio_json['byArtist']['@id'], audio_json['byArtist']['name'])
-          item['content_html'] += '<tr><td><audio controls><source src="{0}" type="audio/mpeg"><a href="{0}">Play track</a></audio></td></tr></table></center>'.format(audio_url)
+            desc += 'from <a href="{}">{}</a><br/>'.format(audio_json['inAlbum']['@id'], audio_json['inAlbum']['name'])
+          desc += 'by <a href="{}">{}</a></small>'.format(audio_json['byArtist']['@id'], audio_json['byArtist']['name'])
+          item['content_html'] = '<center><table style="width:360px; border:1px solid black; border-radius:10px; border-spacing:0;"><tr><td style="width:1%; padding:0; margin:0;"><a href="{}"><img style="display:block; border-top-left-radius:10px; border-bottom-left-radius:10px;" src="{}" /></a></td><td style="padding-left:0.5em; vertical-align:top; display:block; text-overflow:ellipsis; word-wrap:break-word; overflow:hidden; max-height:120px;">{}</td></tr></table></center>'.format(audio_src, poster, desc)
 
         elif audio_json['@type'] == 'MusicAlbum':
-          item['content_html'] = '<center><table style="width:480px;"><tr><td colspan="2"><img width="100%" src="{}"></td></tr>'.format(audio_json['image'])
+          poster = '{}/image?url={}&height=128'.format(config.server, audio_json['image'])
+          desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4>by <a href="{}">{}</a>'.format(audio_json['@id'], audio_json['name'], audio_json['byArtist']['@id'], audio_json['byArtist']['name'])
+          if audio_json.get('description'):
+            desc += '<br/><small>{}</small>'.format(audio_json['description'])
+          item['content_html'] = '<center><table style="width:360px; border:1px solid black; border-radius:10px; border-spacing:0;"><tr><td style="width:1%; padding:0; margin:0;"><a href="{}"><img style="display:block; border-top-left-radius:10px;" src="{}" /></a></td><td style="padding-left:0.5em; vertical-align:top; display:block; text-overflow:ellipsis; word-wrap:break-word; overflow:hidden; max-height:120px;">{}</td></tr>'.format(audio_json['@id'], poster, desc)
+          poster = '{}/image?width=32&height=32&color=none&overlay=audio'.format(config.server)
           for i, track in enumerate(audio_json['track']['itemListElement']):
-            audio_url = '{}/audio?url='.format(config.server, quote_plus(track['item']['@id']))
+            audio_url = '{}/audio?url={}'.format(config.server, quote_plus(track['item']['@id']))
             audio_src = ''
             if track['item'].get('additionalProperty'):
               for prop in track['item']['additionalProperty']:
                 if 'mp3' in prop['name']:
                   audio_src = prop['value']
                   break
-            if audio_src:
-              item['content_html'] += '<tr><td style="width:50%; height:3em;">{0}.&nbsp;<a href="{1}">{2}</a></td><td><audio controls><source src="{3}" type="audio/mpeg"><a href="{3}">Play track</a></audio></td></tr>'.format(i+1, track['item']['@id'], track['item']['name'], audio_url)
+            if i == 0:
+              item['content_html'] += '<tr><td colspan="2" style="border-top:1px solid black; border-collapse:collapse; '
             else:
-              item['content_html'] += '<tr><td colspan="2" style="height:3em;">{0}.&nbsp;<a href="{1}">{2}</a></td><td>'.format(i+1, track['item']['@id'], track['item']['name'])
+              item['content_html'] += '<tr><td colspan="2" style="'
+            if audio_src:
+              item['content_html'] += 'padding-left:24px;"><div style="display:flex; align-items:center;"><a href="{}"><img src="{}" /></a>&nbsp;{}.&nbsp;<a href="{}">{}</a></div></td></tr>'.format(audio_url, poster, track['position'], track['item']['@id'], track['item']['name'])
+            else:
+              item['content_html'] += 'padding-left:72px;">&nbsp;{}.&nbsp;<a href="{}">{}</a></td></tr>'.format(track['position'], track['item']['@id'], track['item']['name'])
           item['content_html'] += '</table></center>'
           if args and 'embed' in args and item.get('summary'):
             item['content_html'] += '<p>{}</p>'.format(item['summary'])
