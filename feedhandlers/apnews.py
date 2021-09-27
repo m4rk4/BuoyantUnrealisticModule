1ea50f7b59f08673acd7edb10a8ffbde85c7880f
m4rk4 <m4rk4@outlook.com>
2021-09-27 18:16:44 -0400

    Fixes and better handling of apnews

--- feedhandlers/apnews.py
+++ feedhandlers/apnews.py
@@ -1,5 +1,7 @@
-import json, os, re
+import re
+from bs4 import BeautifulSoup
 from datetime import datetime, timezone
+from urllib.parse import urlsplit
 
 import utils
 
@@ -7,28 +9,26 @@ import logging
 logger = logging.getLogger(__name__)
 
 def add_media(media):
-  def_img_size = 800
   if media['type'] == 'Photo':
-    sizes = media['imageRenderedSizes'] 
-    size = sizes[min(range(len(sizes)), key = lambda i: abs(sizes[i] - def_img_size))]
+    size = utils.closest_value(media['imageRenderedSizes'], 1000)
     url = media['gcsBaseUrl'] + str(size) + media['imageFileExtension']
     media_html = utils.add_image(url, media['flattenedCaption'])
   elif media['type'] == 'YouTube':
-    media_html = utils.add_youtube(media['externalId'], media['flattenedCaption'])
+    media_html = utils.add_embed('https://www.youtube.com/watch?v={}'.format(media['externalId']))
   else:
-    logger.warning('Unhandled media type {}' + media['type'])
-  return media_html 
+    logger.warning('Unhandled media type {}'.format(media['type']))
+    media_html = ''
+  return media_html
 
 def get_image_url(img_id, file_ext='.jpeg', img_width=600):
   return 'https://storage.googleapis.com/afs-prod/media/{}/{}{}'.format(img_id, img_width, file_ext)
 
-def get_item(content_data, args):
+def get_item(content_data, args, save_debug=False):
   item = {}
   item['id'] = content_data['id']
   item['url'] = content_data['localLinkUrl']
   item['title'] = content_data['headline']
 
-  # Dates
   dt_pub = None
   if content_data.get('published'):
     dt_pub = datetime.fromisoformat(content_data['published']).replace(tzinfo=timezone.utc)
@@ -41,12 +41,11 @@ def get_item(content_data, args):
   if dt_pub:
     item['date_published'] = dt_pub.isoformat()
     item['_timestamp'] = dt_pub.timestamp()
-    item['_display_date'] = dt_pub.strftime('%b %-d, %Y')
+    item['_display_date'] = '{}. {}, {}'.format(dt_pub.strftime('%b'), dt_pub.day, dt_pub.year)
 
   if not utils.check_age(item, args):
     return None
 
-  # Authors
   if content_data.get('bylines'):
     item['author'] = {}
     m = re.search(r'^By\s(.*)', content_data['bylines'], flags=re.I)
@@ -55,179 +54,164 @@ def get_item(content_data, args):
     else:
       item['author']['name'] = content_data['bylines'].title().replace('And', 'and')
 
-  # Tags
   if content_data.get('tagObjs'):
     item['tags'] = []
     for tag in content_data['tagObjs']:
       item['tags'].append(tag['name'])
 
-  # Article image
   if content_data.get('leadPhotoId'):
-    item['image'] = get_image_url(content_data['leadPhotoId'])
+    item['_image'] = get_image_url(content_data['leadPhotoId'])
 
-  # Article summary
   item['summary'] = content_data['flattenedFirstWords']
 
-  # Article html
-  def sub_divs(matchobj):
-    nonlocal args
-    nonlocal content_data
+  story_soup = BeautifulSoup(content_data['storyHTML'], 'html.parser')
 
-    m = re.search(r'class=["\']([^"\']+)["\']', matchobj.group(1))
-    if m:
-      if m.group(1) == 'ad-placeholder':
-        return ''
-      
-      m_id = re.search(r'id=["\']([^"\']+)["\']', matchobj.group(1))
-      if not m_id:
-        logger.warning('can not parse id for placeholder {} in {}'.format(matchobj.group(1), args['url']))
-        return ''
+  for el in story_soup.find_all(class_=['ad-placeholder', 'hub-peek-embed']):
+    el.decompose()
 
-      if m.group(1) == 'media-placeholder':
-        if content_data.get('media'):
-          for it in content_data['media']:
-            if it['id'] == m_id.group(1):
-              return add_media(it)
-        else:
-          return utils.add_image(get_image_url(it['id']))
+  for el in story_soup.find_all(class_='media-placeholder'):
+    for media in content_data['media']:
+      if media['id'] == el['id']:
+        new_html = add_media(media)
+        if new_html:
+          el.insert_after(BeautifulSoup(new_html, 'html.parser'))
+          el.decompose()
+          break
 
-      elif m.group(1) == 'related-story-embed':
-        for it in content_data['relatedStoryEmbeds']:
-          if it['id'] == m_id.group(1):
-            html = '<h4>{}</h4><ul>'.format(it['introText'])
-            for li in it['contentsList']:
-              li_data = utils.get_url_json('https://storage.googleapis.com/afs-prod/contents/' + li['contentId'])
-              if li_data:
-                title = li_data['headline']
-                desc = li_data['flattenedFirstWords']
-              else:
-                title, desc = utils.get_url_title_desc(li['url'])
-              html += '<li><a href="{}">{}</a><br />{}</li>'.format(li['url'], title, desc)
-            html += '</ul>'
-            return html
-  
-      elif m.group(1) == 'hub-link-embed':
-        for it in content_data['richEmbeds']:
-          if it['id'] == m_id.group(1):
-            #print(it['calloutText'])
-            return '<h4>{} &ndash; <a href="https://apnews.com/hub/{}">{}</a></h4>'.format(it['calloutText'], it['tag']['id'], it['displayName'])
-      else:
-        logger.warning('unhandled placeholder {} in {}'.format(m.group(1), args['url']))
+  for el in story_soup.find_all(class_='social-embed'):
+    for embed in content_data['socialEmbeds']:
+      if embed['id'] == el['id']:
+        new_html = ''
+        embed_soup = BeautifulSoup(embed['html'], 'html.parser')
+        if embed_soup.iframe:
+          if embed_soup.iframe['src'].startswith('https://interactives.ap.org/embeds/'):
+            if embed_soup.iframe.get('title'):
+              title = embed_soup.iframe['title']
+            else:
+              title = embed_soup.iframe['src']
+            new_html = '<blockquote><b>Embedded content: <a href="{}">{}</a></b></blockquote>'.format(embed_soup.iframe['src'], title)
+          else:
+            new_html = utils.add_embed(embed_soup.iframe['src'])
+        elif embed_soup.contents[0].name == 'img':
+          new_html = utils.add_image(embed_soup.img['src'])
+        if new_html:
+          el.insert_after(BeautifulSoup(new_html, 'html.parser'))
+          el.decompose()
+        break
 
-    else:
-      logger.warning('unhandled placeholder {} in {}'.format(matchobj.group(0), args['url']))
-    return ''
+  for el in story_soup.find_all(class_='related-story-embed'):
+    for embed in content_data['relatedStoryEmbeds']:
+      if embed['id'] == el['id']:
+        new_html = '<h4>{}</h4><ul>'.format(embed['introText'])
+        for li in embed['contentsList']:
+          title = ''
+          desc = ''
+          if li.get('contentId'):
+            li_json = utils.get_url_json('https://afs-prod.appspot.com/api/v2/content/' + li['contentId'])
+            if li_json:
+              title = li_json['headline']
+              desc = li_json['flattenedFirstWords']
+          else:
+            title, desc = utils.get_url_title_desc(li['url'])
+          new_html += '<li><b><a href="{}">{}</a></b><br />{}</li>'.format(li['url'], title, desc)
+        new_html += '</ul>'
+        el.insert_after(BeautifulSoup(new_html, 'html.parser'))
+        el.decompose()
+        break
 
-  #content_html = re.sub(r'<div([^>]+)>([^<]*)</div>', sub_divs, content_data['storyHTML'])
+  for el in story_soup.find_all(class_='hub-link-embed'):
+    for embed in content_data['richEmbeds']:
+      if embed['id'] == el['id']:
+        new_html = '<blockquote><b>{} &ndash; <a href="https://apnews.com/hub/{}">{}</a></b></blockquote'.format(embed['displayName'], embed['tag']['id'], embed['calloutText'])
+        el.insert_after(BeautifulSoup(new_html, 'html.parser'))
+        el.decompose()
+        break
 
   item['content_html'] = ''
   if content_data.get('leadPhotoId'):
     if content_data.get('media'):
-      for it in content_data['media']:
-        if it['id'] == content_data['leadPhotoId']:
-          item['content_html'] = add_media(it)
+      for media in content_data['media']:
+        if media['id'] == content_data['leadPhotoId']:
+          item['content_html'] = add_media(media)
           break
     else:
       item['content_html'] = utils.add_image(item['image'])
-    # Remove because Inoreader uses the image as a lead
-    del item['image']
 
-  item['content_html'] += re.sub(r'<div([^>]+)>([^<]*)</div>', sub_divs, content_data['storyHTML'])
+  item['content_html'] += str(story_soup)
 
   if content_data.get('media'):
-    for it in content_data['media']:
-      exists = False
-      if re.search(it['id'], item['content_html']):
-        exists = True
-      elif it.get('externalId'):
-        if re.search(it['externalId'], item['content_html']):
-          exists = True
+    for media in content_data['media']:
+      exists = re.search(media['id'], item['content_html'])
+      if not exists and media.get('externalId'):
+        exists = re.search(media['externalId'], item['content_html'])
       if not exists:
-        item['content_html'] += add_media(it)
+        item['content_html'] += add_media(media)
 
   return item
 
 def get_content(url, args, save_debug=False):
-  html = utils.get_url_html(url)
-  if not html:
-    return None
   if save_debug:
-    with open('./debug/debug.html', 'w', encoding='utf-8') as f:
-      f.write(html)
-
-  m = re.search(r"window\[\'titanium-state\'\]\s=\s(.*)\swindow\[\'titanium-cacheConfig\'\]", html, flags=re.S)
+    logger.debug('getting content for ' + url)
+  m = re.search('([0-9a-f]+)\/?$', url)
   if not m:
-    logger.warning('Unable to find article json data in ' + url)
+    logger.warning('unable to parse article id from ' + url)
+    return None
+
+  article_json = utils.get_url_json('https://afs-prod.appspot.com/api/v2/content/' + m.group(1))
+  if not article_json:
     return None
   if save_debug:
-    with open('./debug/debug.txt', 'w', encoding='utf-8') as f:
-      f.write(m.group(1))
+    utils.write_file(article_json, './debug/debug.json')
 
-  try:
-    article_json = json.loads(m.group(1))
-  except:
-    logger.warning('Error loading content json in ' + url)
+  return get_item(article_json, args, save_debug)
+
+def get_feed(args, save_debug=False):
+  split_url = urlsplit(args['url'])
+  if split_url.path.startswith('/hub'):
+    tag = split_url.path.split('/')[2]
+    feed_url = 'https://afs-prod.appspot.com/api/v2/feed/tag?tags={}'.format(tag)
+  else:
+    feed_url = 'https://afs-prod.appspot.com/api/v2/feed/tag'
+
+  feed_json = utils.get_url_json(feed_url)
+  if not feed_json:
     return None
   if save_debug:
-    with open('./debug/debug.json', 'w') as file:
-      json.dump(article_json, file, indent=4)
-
-  for key, data in article_json['content']['data'].items():
-    return get_item(data, args)
-
-def get_feed(args_orig, save_debug=False):
-  args = args_orig.copy()
-  if args.get('tags'):
-    args['url'] = 'https://afs-prod.appspot.com/api/v2/feed/tag?tags=' + args['tags']
-    if not args.get('title'):
-      args['title'] = 'AP News > ' + args['tags']
-    if not args.get('home_page_url'):
-      args['home_page_url'] = 'https://apnews.com/' + args['tags']
-
-  feed = utils.init_jsonfeed(args)
-
-  posts = utils.get_url_json(args['url'])
-  if posts is None:
-    return feed
-  if save_debug:
-    with open('./debug/debug.json', 'w') as file:
-      json.dump(posts, file, indent=4)
+    utils.write_file(feed_json, './debug/debug.json')
 
   # Loop through each post
   n = 0
-  for card in posts['cards']:
+  items = []
+  for card in feed_json['cards']:
     for content in card['contents']:
-      if content.get('gcsUrl'):
-        content_data = utils.get_url_json(content['gcsUrl'])
+      url = 'https://afs-prod.appspot.com/api/v2/content/{}'.format(content['id'])
+      if content['contentType'] == 'text':
+        item = get_content(url, args, save_debug)
+        if item:
+          if utils.filter_item(item, args) == True:
+            items.append(item)
       else:
-        content_data = utils.get_url_json('https://storage.googleapis.com/afs-prod/contents/' + content['id'])
-      if content_data:
-        item = get_item(content_data, args)
-      else:
-        item = get_item(content, args)
-      if item:
-        if utils.filter_item(item, args) == True:
-          feed['items'].append(item)
-        n += 1
-        if 'max' in args:
-          if n == int(args['max']):
-            return feed
+        logger.warning('unhandled contentType {} in {}'.format(content['contentType'], url))
 
     for card_feed in card['feed']:
       for content in card_feed['contents']:
-        if content.get('gcsUrl'):
-          content_data = utils.get_url_json(content['gcsUrl'])
+        url = 'https://afs-prod.appspot.com/api/v2/content/{}'.format(content['id'])
+        if content['contentType'] == 'text':
+          item = get_content(url, args, save_debug)
+          if item:
+            if utils.filter_item(item, args) == True:
+              items.append(item)
         else:
-          content_data = utils.get_url_json('https://storage.googleapis.com/afs-prod/contents/' + content['id'])
-        if content_data:
-          item = get_item(content_data, args)
-        else:
-          item = get_item(content, args)
-        if item:
-          if utils.filter_item(item, args) == True:
-            feed['items'].append(item)
-            n += 1
-            if 'max' in args:
-              if n == int(args['max']):
-                return feed
+          logger.warning('unhandled contentType {} in {}'.format(content['contentType'], url))
+
+  # sort by date
+  items = sorted(items, key=lambda i: i['_timestamp'], reverse=True)
+
+  feed = utils.init_jsonfeed(args)
+
+  if 'max' in args:
+    n = int(args['max'])
+    feed['items'] = items[:n].copy()
+  else:
+    feed['items'] = items.copy()
   return feed
