1cb3b396a6b24fba8f4b0fcca8afc813c0cc1fc6
m4rk4 <m4rk4@outlook.com>
2021-11-15 21:43:05 -0500

    format TikTok embeds like Instagram

--- feedhandlers/tiktok.py
+++ feedhandlers/tiktok.py
@@ -28,7 +28,7 @@ def get_item(content, args, save_debug=False):
     item['author'] = {}
     item['author']['name'] = '{} (@{})'.format(content['authorInfos']['nickName'], content['authorInfos']['uniqueId'])
     avatar = '{}/image?url={}&height=48&mask=ellipse'.format(config.server, quote_plus(content['authorInfos']['covers'][0]))
-    author_info = '<a href="https://www.tiktok.com/@{0}"><b>{0}</b></a>&nbsp;<small>{1}</small>'.format(content['authorInfos']['uniqueId'], content['authorInfos']['nickName'])
+    author_info = '<a href="https://www.tiktok.com/@{0}"><b>{0}</b></a><br/><small>{1}</small>'.format(content['authorInfos']['uniqueId'], content['authorInfos']['nickName'])
 
     if content.get('textExtra'):
       item['tags'] = []
@@ -54,7 +54,7 @@ def get_item(content, args, save_debug=False):
     item['author'] = {}
     item['author']['name'] = '{} (@{})'.format(content['author']['nickname'], content['author']['uniqueId'])
     avatar = '{}/image?url={}&height=48&mask=ellipse'.format(config.server, quote_plus(content['author']['avatarThumb']))
-    author_info = '<a href="https://www.tiktok.com/@{0}"><b>{0}</b></a>&nbsp;<small>{1}</small>'.format(content['author']['uniqueId'], content['author']['nickname'])
+    author_info = '<a href="https://www.tiktok.com/@{0}"><b>{0}</b></a><br/><small>{1}</small>'.format(content['author']['uniqueId'], content['author']['nickname'])
 
     if content.get('textExtra'):
       item['tags'] = []
@@ -68,14 +68,16 @@ def get_item(content, args, save_debug=False):
 
     item['summary'] = re.sub(r'(@|#)(\w+)', replace_entity, content['desc'], flags=re.I)
 
-  item['content_html'] = '<table style="width:500px !important; margin-left:auto; margin-right:auto; padding:10px; border:1px solid black; border-radius:10px; font-family:Roboto,Helvetica,Arial,sans-serif;"><tr><td style="width:50px;"><img style="vertical-align: middle;" src="{}" /></td><td>{}</td></tr>'.format(avatar, author_info)
-  item['content_html'] += '<tr><td>&nbsp;</td><td>{}</td></tr>'.format(item['summary'])
+  item['content_html'] = '<div style="width:488px; padding:8px 0 8px 8px; border:1px solid black; border-radius:10px; font-family:Roboto,Helvetica,Arial,sans-serif;"><div><img style="float:left; margin-right:8px;" src="{}"/><div>{}</div><div style="clear:left;"></div></div><p>{}</p>'.format(avatar, author_info, item['summary'])
 
   if music_info:
     music_svg = '<svg width="18" height="18" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="transform: translateY(4px);"><path fill-rule="evenodd" clip-rule="evenodd" d="M35.0001 10.7587C35.0001 10.1169 34.4041 9.64129 33.7784 9.78359L17.7902 13.4192C17.335 13.5227 17.0119 13.9275 17.0119 14.3943V37.9972H17.0109C17.0374 40.1644 14.8022 42.4189 11.612 43.2737C8.05093 44.2279 4.64847 43.0769 4.01236 40.7028C3.37624 38.3288 5.74735 35.6308 9.30838 34.6766C10.606 34.3289 11.8826 34.2608 13.0119 34.4294V14.3943C13.0119 12.0601 14.6271 10.0364 16.9033 9.5188L32.8914 5.88317C36.0204 5.17165 39.0001 7.54986 39.0001 10.7587V33.1191C39.084 35.3108 36.8331 37.6109 33.6032 38.4763C30.0421 39.4305 26.6397 38.2795 26.0036 35.9055C25.3675 33.5315 27.7386 30.8334 31.2996 29.8792C32.5961 29.5319 33.8715 29.4635 35.0001 29.6316V10.7587Z"></path></svg>'
-    item['content_html'] += '<tr><td>&nbsp;</td><td>{}&nbsp;{}</td></tr>'.format(music_svg, music_info)
+    item['content_html'] += '<p>{}&nbsp;{}</p>'.format(music_svg, music_info)
 
-  item['content_html'] += '<tr><td>&nbsp;</td><td>{}</td></tr></table>'.format(utils.add_video(item['_video'], 'video/mp4', item['_image'], img_style='border-radius:10px;', fig_style=' margin:0; padding:0;'))
+  #item['content_html'] += '<tr><td>&nbsp;</td><td>{}</td></tr></table>'.format(utils.add_video(item['_video'], 'video/mp4', item['_image'], fig_style=' margin:0; padding:0;'))
+
+  item['content_html'] += utils.add_video(item['_video'], 'video/mp4', item['_image'], width=480, fig_style=' margin:0; padding:0;')
+  item['content_html'] += '<br/><a href="{}"><small>Open in TikTok</small></a></div>'.format(item['url'])
   return item
 
 def get_content(url, args, save_debug=False):
