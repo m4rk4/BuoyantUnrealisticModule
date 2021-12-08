b7204ede2d3cd589943ba8adcadb7c28e5c1b66c
m4rk4 <m4rk4@outlook.com>
2021-12-07 21:48:30 -0500

    New content handlers for art19.com and discovermagazine.com

---
+++ feedhandlers/art19.py
@@ -0,0 +1,142 @@
+import feedparser, math, re
+from datetime import datetime, timezone
+from urllib.parse import quote_plus
+
+import config, utils
+
+import logging
+logger = logging.getLogger(__name__)
+
+def calc_duration(sec):
+  duration = []
+  t = math.floor(float(sec) / 3600)
+  if t >= 1:
+    duration.append('{} hr'.format(t))
+  t = math.ceil((float(sec) - 3600 * t) / 60)
+  if t > 0:
+    duration.append('{} min.'.format(t))
+  return duration
+
+def get_content(url, args, save_debug=False):
+  show = ''
+  m = re.search(r'\/shows\/([^\/]+)', url)
+  if m:
+    show = m.group(1)
+  episode = ''
+  m = re.search(r'\/episodes\/([^\/]+)', url)
+  if m:
+    episode = m.group(1)
+
+  if episode:
+    headers = {
+      "Accept": "application/json",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9,de;q=0.8",
+      "Connection": "keep-alive",
+      "Host": "rss.art19.com",
+      "Origin": "https://art19.com",
+      "Referer": "https://art19.com/",
+      "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Microsoft Edge\";v=\"96\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-origin",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43"
+    }
+    ep_json = utils.get_url_json('https://rss.art19.com/episodes/{}?content_only=true'.format(episode), headers=headers)
+    if not ep_json:
+      return None
+    if save_debug:
+      utils.write_file(ep_json, './debug/audio.json')
+
+    item = {}
+    item['id'] = ep_json['content']['episode_id']
+    item['url'] = ep_json['content']['episode_share_url']
+    item['title'] = ep_json['content']['episode_title']
+
+    dt = datetime.fromisoformat(ep_json['performed_at'].replace('Z', '+00:00'))
+    item['date_published'] = dt.isoformat()
+    item['_timestamp'] = dt.timestamp()
+    item['_display_date'] = '{}. {}, {}'.format(dt.strftime('%b'), dt.day, dt.year)
+
+    item['author'] = {}
+    item['author']['name'] = ep_json['content']['series_title']
+
+    if ep_json['content']['artwork'].get('episode'):
+      images = ep_json['content']['artwork']['episode']
+    else:
+      images = ep_json['content']['artwork']['show']
+    img = utils.closest_dict(images, 'height', 640)
+    item['_image'] = img['url']
+
+    item['_audio'] = ep_json['content']['media']['mp3']['url']
+
+    item['summary'] = ep_json['content']['episode_description']
+
+    duration = calc_duration(ep_json['content']['duration'])
+    poster = '{}/image?height=128&url={}&overlay=audio'.format(config.server, quote_plus(item['_image']))
+    desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4><small>by <a href="{}">{}</a><br/>{}<br/>{}</small>'.format(item['url'], item['title'], ep_json['content']['series_show_page'], item['author']['name'], item['_display_date'], ', '.join(duration))
+    item['content_html'] = '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div>{}</div><div style="clear:left;">'.format(item['_audio'], poster, desc)
+    if not 'embed' in args:
+      item['content_html'] += '<blockquote><small>{}</small></blockquote>'.format(item['summary'])
+    item['content_html'] += '</div>'
+  else:
+    try:
+      d = feedparser.parse('https://rss.art19.com/' + show)
+    except:
+      logger.warning('Feedparser error https://rss.art19.com/' + show)
+      return None
+
+    item = {}
+    item['id'] = d['feed']['link']
+    item['url'] = d['feed']['link']
+    item['title'] = d['feed']['title']
+
+    ts = d['entries'][0]['published_parsed']
+    dt = datetime(*ts[0:7]).replace(tzinfo=timezone.utc)
+    item['date_published'] = dt.isoformat()
+    item['_timestamp'] = dt.timestamp()
+    item['_display_date'] = '{}. {}, {}'.format(dt.strftime('%b'), dt.day, dt.year)
+
+    item['author'] = {"name": d['feed']['author']}
+
+    if d['feed'].get('tags'):
+      item['tags'] = []
+      for tag in d['feed']['tags']:
+        item['tags'].append(tag['term'])
+
+    if d['feed'].get('image'):
+      item['_image'] = d['feed']['image']['href']
+
+    if d['feed'].get('summary'):
+      item['summary'] = d['feed']['summary']
+
+    poster = '{}/image?height=128&url={}'.format(config.server, quote_plus(item['_image']))
+    desc = '<h3 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h3>by {}'.format(item['url'], item['title'], item['author']['name'])
+    item['content_html'] = '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div>{}</div><div style="clear:left;"><br/><blockquote>'.format(item['url'], poster, desc)
+    if 'max' in args:
+      max = args['max'] - 1
+    else:
+      max = 2
+    for i,entry in enumerate(d['entries']):
+      episode = ''
+      for link in entry['links']:
+        m = re.search(r'\/episodes\/([a-f0-9\-]+)', link['href'])
+        if m:
+          episode = m.group(1)
+          break
+      t = entry['itunes_duration'].split(':')
+      duration = calc_duration(int(t[0])*3600 + int(t[1])*60 + int(t[2]))
+      poster = '{}/image?height=96&url={}&overlay=audio'.format(config.server, quote_plus(entry['image']['href']))
+      ts = entry['published_parsed']
+      dt = datetime(*ts[0:7]).replace(tzinfo=timezone.utc)
+      display_date = '{}. {}, {}'.format(dt.strftime('%b'), dt.day, dt.year)
+      desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}/episodes/{}">{}</a></h4><small>{}<br/>{}</small>'.format(item['url'], episode, entry['title'], display_date, ', '.join(duration))
+      item['content_html'] += '<div><a href="https://rss.art19.com/episodes/{}.mp3"><img style="float:left; margin-right:8px;" src="{}"/></a><div>{}</div><div style="clear:left;"><br/>'.format(episode, poster, desc)
+      if i == max:
+        break
+    if i == max:
+      item['content_html'] += '<a href="{}">See more episodes...</a>'
+    item['content_html'] += '</blockquote></div>'
+  return item
---
+++ feedhandlers/discovermag.py
@@ -0,0 +1,187 @@
+import re
+from datetime import datetime, timezone
+from urllib.parse import urlsplit
+
+import utils
+from feedhandlers import rss
+
+import logging
+logger = logging.getLogger(__name__)
+
+def resize_image(img_src, width=1000):
+  split_url = urlsplit(img_src)
+  return 'https://{}{}?fm=jpg&fl=progressive&w={}&fit=pad'.format(split_url.netloc, split_url.path, width)
+
+def get_next_json(url):
+  sites_json = utils.read_json_file('./sites.json')
+  next_url = 'https://www.discovermagazine.com/_next/data/' + sites_json['discovermagazine']['buildId']
+
+  split_url = urlsplit(url)
+  if split_url.path:
+    next_url += split_url.path + '.json'
+  else:
+    next_url += '/index.json'
+
+  next_json = utils.get_url_json(next_url, retries=1)
+  if not next_json:
+    logger.debug('updating discovermagazine.com buildId')
+    article_html = utils.get_url_html(url)
+    m = re.search(r'"buildId":"([a-f0-9]+)"', article_html)
+    if m:
+      sites_json['discovermagazine']['buildId'] = m.group(1)
+      utils.write_file(sites_json, './sites.json')
+      next_json = utils.get_url_json('https://www.discovermagazine.com/_next/data/{}{}.json'.format(m.group(1), split_url.path))
+      if not next_json:
+        return None
+  return next_json
+
+def get_content(url, args, save_debug):
+  next_json = get_next_json(url)
+  if not next_json:
+    return None
+  if save_debug:
+    utils.write_file(next_json, './debug/debug.json')
+
+  article_json = next_json['pageProps']['content']['article']
+  item = {}
+  item['id'] = article_json['id']
+  item['url'] = url
+  item['title'] = article_json['title']
+
+  dt = datetime.fromisoformat(article_json['meta']['publicationDate']).astimezone(timezone.utc)
+  item['date_published'] = dt.isoformat()
+  item['_timestamp'] = dt.timestamp()
+  item['_display_date'] = '{}. {}, {}'.format(dt.strftime('%b'), dt.day, dt.year)
+  dt = datetime.fromisoformat(article_json['meta']['modifiedDate'].replace('Z', '+00:00'))
+  item['date_modified'] = dt.isoformat()
+
+  # Check age
+  if args.get('age'):
+    if not utils.check_age(item, args):
+      return None
+
+  authors = []
+  for author in article_json['refs']['authors']:
+    authors.append(author['name'])
+  item['author'] = {}
+  item['author']['name'] = re.sub(r'(,)([^,]+)$', r' and\2', ', '.join(authors))
+
+  item['tags'] = []
+  item['tags'].append(article_json['refs']['category']['name'])
+  if article_json['refs'].get('tags'):
+    for tag in article_json['refs']['tags']:
+      item['tags'].append(tag['name'])
+
+  item['_image'] = resize_image(article_json['image']['url'])
+
+  if article_json.get('subtitle'):
+    item['summary'] = article_json['subtitle']
+
+  if article_json['image'].get('description'):
+    caption = article_json['image']['description']
+  else:
+    caption = ''
+  item['content_html'] = utils.add_image(resize_image(item['_image']), caption)
+
+  def process_content(content):
+    nonlocal url
+    content_html = ''
+    if content['nodeType'] == 'paragraph':
+      content_html = '<p>'
+      for c in content['content']:
+        content_html += process_content(c)
+      content_html += '</p>'
+
+    elif content['nodeType'] == 'text':
+      content_html = content['value']
+      for mark in content['marks']:
+        if mark['type'] == 'bold':
+          content_html = '<b>{}</b>'.format(content_html)
+        elif mark['type'] == 'italic':
+          content_html = '<i>{}</i>'.format(content_html)
+        elif mark['type'] == 'underline':
+          content_html = '<u>{}</u>'.format(content_html)
+        else:
+          logger.warning('unhandled mark type {} in {}'.format(mark['type'], url))
+
+    elif content['nodeType'] == 'hyperlink':
+      content_html = '<a href="{}">'.format(content['data']['uri'])
+      for c in content['content']:
+        content_html += process_content(c)
+      content_html += '</a>'
+
+    elif content['nodeType'].startswith('heading-'):
+      content_html = '<h{}>'.format(content['nodeType'][-1])
+      for c in content['content']:
+        content_html += process_content(c)
+      content_html += '</h{}>'.format(content['nodeType'][-1])
+
+    elif content['nodeType'].endswith('-list'):
+      if content['nodeType'].startswith('ordered'):
+        tag = 'ol'
+      else:
+        tag = 'ul'
+      content_html = '<{}>'.format(tag)
+      for c in content['content']:
+        content_html += process_content(c)
+      content_html += '</{}>'.format(tag)
+
+    elif content['nodeType'] == 'list-item':
+      content_html = '<li>'
+      for c in content['content']:
+        content_html += process_content(c)
+      content_html += '</li>'
+
+    elif content['nodeType'] == 'embedded-asset-block':
+      if content['data']['target']['fields'].get('file') and 'image' in content['data']['target']['fields']['file']['contentType']:
+        if content['data']['target']['fields'].get('description'):
+          caption = content['data']['target']['fields']['description']
+        else:
+          caption = ''
+        content_html = utils.add_image(resize_image(content['data']['target']['fields']['file']['url']), caption)
+      else:
+        logger.warning('unhandled embedded-asset-block in ' + url)
+
+    elif content['nodeType'] == 'embedded-entry-block':
+      if content['data']['target']['fields'].get('videoUrl'):
+        content_html = utils.add_embed(content['data']['target']['fields']['videoUrl'])
+      else:
+        logger.warning('unhandled embedded-entry-block in ' + url)
+
+    elif content['nodeType'] == 'hr':
+      content_html = '<hr/>'
+
+    else:
+      logger.warning('unhandled nodeType {} in {}'.format(content['nodeType'], url))
+
+    return content_html
+
+  for c in article_json['body']['content']:
+    item['content_html'] += process_content(c)
+
+  return item
+
+def get_feed(args, save_debug):
+  # https://www.discovermagazine.com/rss/all
+  if '/rss/' in args['url']:
+    return rss.get_feed(args, save_debug, get_content)
+
+  # Use category url: https://www.discovermagazine.com/technology
+  next_json = get_next_json(args['url'])
+  if not next_json:
+    return None
+  if save_debug:
+    utils.write_file(next_json, './debug/feed.json')
+
+  feed = utils.init_jsonfeed(args)
+  for article in next_json['pageProps']['content']['articles']:
+    if not article.get('slug'):
+      continue
+    article_url = 'https://www.discovermagazine.com/{}/{}'.format(article['refs']['category']['slug'], article['slug'])
+    if save_debug:
+      logger.debug('getting content for ' + article_url)
+    item = get_content(article_url, args, save_debug)
+    if item:
+      if utils.filter_item(item, args) == True:
+        feed['items'].append(item)
+  return feed
