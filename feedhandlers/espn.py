11b4a0cbdbc951eff52acb82c0c0ee9cb6c2e54e
m4rk4 <m4rk4@outlook.com>
2021-12-22 17:06:39 -0500

    - Handle inlines at the api level - Handle blog article types

--- feedhandlers/espn.py
+++ feedhandlers/espn.py
@@ -25,12 +25,18 @@ def get_video(video):
   vid_type = 'video/mp4'
   poster = resize_image(video['thumbnail'])
   # poster = video['posterImages']['full']['href']
-  caption = []
+  caption = ''
+  if video.get('headline'):
+    caption += '<a href="{}"><b>{}</b></a>'.format(video['links']['web']['href'], video['headline'])
   if video.get('caption'):
-    caption.append(video['caption'])
+    if caption:
+      caption += '. '
+    caption += video['caption']
   if video.get('credit'):
-    caption.append(format(video['credit']))
-  return vid_src, vid_type, poster, ' | '.join(caption)
+    if caption:
+      caption += ' | '
+    caption += video['credit']
+  return vid_src, vid_type, poster, caption
 
 def get_story(story_json, args, save_debug=False):
   item = {}
@@ -64,25 +70,35 @@ def get_story(story_json, args, save_debug=False):
   else:
     item['author']['name'] = 'ESPN'
 
+  item['tags'] = []
   if story_json.get('keywords'):
     item['tags'] = story_json['keywords'].copy()
-  else:
-    item['tags'] = []
+  if story_json.get('categories'):
     for tag in story_json['categories']:
       if tag.get('description'):
-        item['tags'].append(tag['description'])
+        if not tag['description'] in item['tags']:
+          item['tags'].append(tag['description'])
+  if story_json.get('section') and not story_json['section'] in item['tags']:
+    item['tags'].append(story_json['section'])
 
   if story_json.get('images'):
     item['_image'] = story_json['images'][0]['url']
 
   item['summary'] = story_json['description']
 
-  story_soup = BeautifulSoup(story_json['story'], 'html.parser')
+  story_html = story_json['story']
+  if story_json['type'] == 'BlogEntry':
+    story_html = re.sub(r'^<br \/>([^\s])', r'<p>\1', story_html, flags=re.M)
+    story_html = re.sub(r'^<br \/>\s', '</p>', story_html, flags=re.M)
+    story_html = re.sub(r'<!--(\w+)-->', r'<\1></\1>', story_html)
+  elif story_json['type'] == 'Preview':
+    story_html = story_html.replace('\n\n\r\n', '<br/><br/>')
+  story_soup = BeautifulSoup(story_html, 'html.parser')
   if save_debug:
     utils.write_file(str(story_soup), './debug/debug.html')
 
   for el in story_soup.find_all(class_='floatleft'):
-    el['style'] = 'float:left; margin-top:0.25em; margin-right:0.5em;'
+    el['style'] = 'float:left; margin-right:0.5em;'
     it = story_soup.new_tag('div')
     it['style'] = 'clear:left;'
     a = el.find_next('h2')
@@ -104,71 +120,83 @@ def get_story(story_json, args, save_debug=False):
     el.insert_after(it)
     el.decompose()
 
-  for el in story_soup.find_all(re.compile('photo\d+')):
-    m = re.search(r'\d+$', el.name)
-    n = int(m.group(0)) - 1
-    image = story_json['images'][n]
-    img_src, caption = get_image(image)
-    new_html = utils.add_image(resize_image(img_src), caption)
-    el.insert_after(BeautifulSoup(new_html, 'html.parser'))
-    el.decompose()
-
-  for el in story_soup.find_all(re.compile('video\d+')):
-    m = re.search(r'\d+$', el.name)
-    n = int(m.group(0)) - 1
-    if n < len(story_json['video']):
-      video = story_json['video'][n]
-      vid_src, vid_type, poster, caption = get_video(video)
-      new_html = utils.add_video(vid_src, vid_type, poster, caption)
+  if story_json.get('images'):
+    for el in story_soup.find_all(re.compile('photo\d+')):
+      m = re.search(r'\d+$', el.name)
+      if m:
+        n = int(m.group(0)) - 1
+      else:
+        n = 0
+      image = story_json['images'][n]
+      img_src, caption = get_image(image)
+      new_html = utils.add_image(resize_image(img_src), caption)
       el.insert_after(BeautifulSoup(new_html, 'html.parser'))
       el.decompose()
-    else:
-      logger.warning('video index out of range in ' + url)
+  else:
+    for el in story_soup.find_all(re.compile('photo\d*')):
+      logger.warning('unhandled photo element {} in {}'.format(el.name, url))
+      el.decompose()
 
-  inlines = None
-  if story_soup.find(re.compile('inline\d+')):
-    # Fetch article html to parse the inlines
-    article_html = utils.get_url_html(url)
-    if not article_html:
-      logger.warning('unable to parse inlines in ' + url)
-    article_soup = BeautifulSoup(article_html, 'html.parser')
-    inlines = article_soup.find_all(class_=['instagram-media', 'twitter-tweet', 'pull-quote', 'module-iframe-wrapper', 'inline'])
-
-  if inlines:
-    for el in story_soup.find_all(re.compile('inline\d+')):
+  if story_json.get('video'):
+    for el in story_soup.find_all(re.compile('video\d*')):
       m = re.search(r'\d+$', el.name)
-      n = int(m.group(0)) - 1
-      if n > len(inlines):
-        logger.warning('inline index out of range ' + url)
-        continue
-      it = inlines[n]
-      new_html = ''
-      if 'instagram-media' in it['class']:
-        new_html = utils.add_embed(it['data-instgrm-permalink'])
-
-      elif 'twitter-tweet' in it['class']:
-        a = a.find_all('a')
-        new_html = utils.add_embed(a[-1]['href'])
-
-      elif 'pull-quote' in it['class']:
-        author = ''
-        a = it.find('cite')
-        if a:
-          author = a.get_text().strip()
-          a.decompose()
-        quote = re.sub(r'^"|"$', '', a.get_text().strip())
-        new_html = utils.add_pullquote(quote, author)
-
-      elif 'module-iframe-wrapper' in it['class']:
-        a = it.find('iframe')
-        new_html = utils.add_embed(a['src'])
-
-      elif 'inline-track' in it['class'] or it.name == 'aside':
-        pass
-
-      if new_html:
+      if m:
+        n = int(m.group(0)) - 1
+      else:
+        n = 0
+      if n < len(story_json['video']):
+        video = story_json['video'][n]
+        vid_src, vid_type, poster, caption = get_video(video)
+        new_html = utils.add_video(vid_src, vid_type, poster, caption)
         el.insert_after(BeautifulSoup(new_html, 'html.parser'))
         el.decompose()
+      else:
+        logger.warning('video index out of range in ' + url)
+  else:
+    for el in story_soup.find_all(re.compile('video\d*')):
+      logger.warning('unhandled video element {} in {}'.format(el.name, url))
+      el.decompose()
+
+  if story_json.get('inlines'):
+    for el in story_soup.find_all(re.compile('inline\d*')):
+      m = re.search(r'\d+$', el.name)
+      if m:
+        n = int(m.group(0)) - 1
+      else:
+        n = 0
+      if n < len(story_json['inlines']):
+        new_html = ''
+        inline = story_json['inlines'][n]
+        if inline['type'] == 'iFrame':
+          inline_soup = BeautifulSoup(inline['body'], 'html.parser')
+          if inline_soup.blockquote and 'twitter-tweet' in inline_soup.blockquote['class']:
+            it = inline_soup.blockquote.find_all('a')
+            new_html = utils.add_embed(it[-1]['href'])
+          elif inline_soup.blockquote and 'instagram-media' in inline_soup.blockquote['class']:
+            new_html = utils.add_embed(inline_soup.blockquote['data-instgrm-permalink'])
+          else:
+            logger.warning('unhandled inline iframe {} in {}'.format(el.name, url))
+
+        elif inline['type'] == 'Module':
+          if inline['moduleType'] == 'pullquote':
+            new_html = utils.add_pullquote(inline['body'], inline['byline'])
+          elif inline['moduleType'] == 'default':
+            pass
+          else:
+            logger.warning('unhandled inline module type {} in {}'.format(inline['moduleType'], url))
+
+        else:
+          logger.warning('unhandled inline type {} in {}'.format(inline['type'], url))
+
+        if new_html:
+          el.insert_after(BeautifulSoup(new_html, 'html.parser'))
+          el.decompose()
+      else:
+        logger.warning('inline index out of range in ' + url)
+  else:
+    for el in story_soup.find_all(re.compile('inline\d*')):
+      logger.warning('unhandled inline element {} in {}'.format(el.name, url))
+      el.decompose()
 
   for el in story_soup.find_all(['alsosee', 'offer']):
     if el.parent and el.parent.name == 'p':
@@ -179,7 +207,7 @@ def get_story(story_json, args, save_debug=False):
   item['content_html'] = ''
 
   # Add lead video or image
-  m = re.search(r'<photo\d+>|<video\d+>', story_json['story'][:20])
+  m = re.search(r'<figure', str(story_soup)[:20])
   if not m:
     if story_json.get('video'):
       vid_src, vid_type, poster, caption = get_video(story_json['video'][0])
@@ -192,23 +220,36 @@ def get_story(story_json, args, save_debug=False):
   return item
 
 def get_content(url, args, save_debug=False):
-  m = re.search(r'\/id\/(\d+)\/', url)
-  if not m:
-    story_html = utils.get_url_html(url)
-    if story_html:
-      m = re.search(r'data-id="(\d+)"', story_html)
-  if not m:
-    logger.warning('unable to parse story id in ' + url)
-    return None
-
-  json_url = 'http://now.core.api.espn.com/v1/sports/news/' + m.group(1)
-  now_json = utils.get_url_json(json_url)
-  if not now_json:
-    return None
+  if '/blog/' in url:
+    api_url = utils.clean_url(url) + '?partial=article&xhr=1&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full&userab=0'
+    api_json = utils.get_url_json(api_url)
+    if api_json:
+      story_json = api_json['content']
+    else:
+      return None
+  else:
+    story_id = ''
+    m = re.search(r'\d+', url.split('_')[1].split('/')[2])
+    if m:
+      story_id = m.group(0)
+    else:
+      story_html = utils.get_url_html(url)
+      if story_html:
+        m = re.search(r'showStory\?uid=(\d+)', story_html)
+        if m:
+          story_id = m.group(1)
+    if not story_id:
+      logger.warning('unable to parse story id in ' + url)
+      return None
+    api_url = 'http://now.core.api.espn.com/v1/sports/news/{}?enable=inlines'.format(story_id)
+    api_json = utils.get_url_json(api_url)
+    if api_json:
+      story_json = api_json['headlines'][0]
+    else:
+      return None
   if save_debug:
-    utils.write_file(now_json, './debug/debug.json')
-
-  return get_story(now_json['headlines'][0], args, save_debug)
+    utils.write_file(story_json, './debug/debug.json')
+  return get_story(story_json, args, save_debug)
 
 def get_feed(args, save_debug=False):
   # API: https://www.espn.com/apis/devcenter/docs/headlines.html
@@ -218,11 +259,11 @@ def get_feed(args, save_debug=False):
   # Steelers: https://now.core.api.espn.com/v1/sports/football/nfl/teams/23/news
   # Penguins: https://now.core.api.espn.com/v1/sports/hockey/nhl/teams/16/news
 
-  feed_json = utils.get_url_json(args['url'])
+  feed_json = utils.get_url_json(args['url'] + '?enable=inlines')
   if not feed_json:
     return None
   if save_debug:
-    utils.write_file(feed_json, './debug/debug.json')
+    utils.write_file(feed_json, './debug/feed.json')
 
   n = 0
   items = []
