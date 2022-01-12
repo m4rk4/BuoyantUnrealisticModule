baab6ee165bc5b992f179bee55383e679b94c30f
m4rk4 <m4rk4@outlook.com>
2022-01-12 16:29:08 -0500

    Add some helper functions

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import importlib, json, math, os, random, re, requests, string, tldextract
+import importlib, io, json, math, os, pytz, random, re, requests, string, tldextract
 from bs4 import BeautifulSoup
 from datetime import datetime
 from PIL import ImageFile
@@ -248,6 +248,15 @@ def read_json_file(json_filename):
     logger.warning('empty file {}'.format(json_filename))
   return json_file
 
+def read_file(filename):
+  content = ''
+  if os.stat(filename).st_size > 0:
+    f = io.open(filename, mode="r", encoding="utf-8")
+    content = f.read()
+  else:
+    logger.warning('empty file {}'.format(filename))
+  return content
+
 def closest_value(lst, target):
   return lst[min(range(len(lst)), key = lambda i: abs(int(lst[i]) - target))]
 
@@ -291,6 +300,10 @@ def init_jsonfeed(args):
 
   feed['items'] = []
   return feed
+
+def format_display_date(dt):
+  dt_est = dt.astimezone(pytz.timezone(config.local_tz))
+  return '{}. {}, {}, {}:{} {} EST'.format(dt_est.strftime('%b'), dt_est.day, dt_est.year, int(dt_est.strftime('%I')), dt_est.strftime('%M'), dt_est.strftime('%p').lower())
 
 def random_alphanumeric_string(str_len=8):
   letters_digits = string.ascii_letters + string.digits
