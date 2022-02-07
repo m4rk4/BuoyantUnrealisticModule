768bf13da04fd924d3ed409d93a36abcc72fc368
m4rk4 <m4rk4@outlook.com>
2022-02-07 15:32:57 -0500

    Content formatting

--- feedhandlers/iheart.py
+++ feedhandlers/iheart.py
@@ -45,21 +45,24 @@ def get_content(url, args, save_debug=False):
     m = re.search(r'/podcast/[^/]+-(\d+)', clean_url)
     if m:
         show = utils.get_url_json('https://us.api.iheart.com/api/v3/podcast/podcasts/' + m.group(1))
+        if save_debug:
+            utils.write_file(show, './debug/podcast.json')
 
     episode = None
     m = re.search(r'/episode/[^/]+-(\d+)', clean_url)
     if m:
         episode = utils.get_url_json('https://us.api.iheart.com/api/v3/podcast/episodes/' + m.group(1))
+        if save_debug:
+            utils.write_file(show, './debug/iheart.json')
 
     if episode and show:
         item = get_episode(episode['episode'], show)
         show_url = 'https://www.iheart.com/podcast/{}-{}/'.format(show['slug'], show['id'])
         poster = '{}/image?height=128&url={}&overlay=audio'.format(config.server, quote_plus(item['_image']))
         desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4><small>by <a href="https://www.iheart.com{}">{}</a><br/>{} &#8226; {}</small>'.format(item['url'], item['title'], show_url, item['author']['name'], item['_display_date'], item['_duration'])
-        item['content_html'] = '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div>{}</div><div style="clear:left;"></div>'.format(item['_audio'], poster, desc)
+        item['content_html'] = '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div style="overflow:hidden;">{}</div><div style="clear:left;"></div></div>'.format(item['_audio'], poster, desc)
         if not 'embed' in args:
-            item['content_html'] += '<blockquote style="border-left:3px solid #ccc; margin-top:4px; margin-left:1.5em; padding-left:0.5em;"><small>{}</small></blockquote></div>'.format(item['summary'])
-        item['content_html'] += '</div>'
+            item['content_html'] += '<blockquote style="border-left:3px solid #ccc; margin-top:4px; margin-left:1.5em; padding-left:0.5em;"><small>{}</small></blockquote>'.format(item['summary'])
 
     elif show and not episode:
         item = {}
@@ -74,18 +77,28 @@ def get_content(url, args, save_debug=False):
         item['summary'] = show['description']
 
         poster = '{}/image?url={}&height=128'.format(config.server, quote_plus(item['_image']))
-        desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4><small>{}</small>'.format(item['url'], item['title'], item['summary'])
-        item['content_html'] = '<div><img style="float:left; margin-right:8px;" src="{}"/><div>{}</div><div style="clear:left;">&nbsp;</div></div>'.format(poster, desc)
+        desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4>'.format(item['url'], item['title'])
+        item['content_html'] = '<div><img style="float:left; margin-right:8px;" src="{}"/><div>{}</div><div style="clear:left;"></div></div>'.format(poster, desc)
 
+        item['content_html'] += '<blockquote style="border-left:3px solid #ccc; margin-top:4px; margin-left:1.5em; padding-left:0.5em;"><h4 style="margin-top:0; margin-bottom:1em;">Episodes:</h4>'
         episodes = utils.get_url_json('https://us.api.iheart.com/api/v3/podcast/podcasts/{}/episodes?newEnabled=false&limit=5&sortBy=startDate-desc'.format(show['id']))
         ts = []
-        for ep in episodes['data']:
+        if 'embed' in args:
+            n = 5
+        else:
+            n = 10
+        for i, ep in enumerate(episodes['data']):
+            if i == n:
+                break
             episode = utils.get_url_json('https://us.api.iheart.com/api/v3/podcast/episodes/{}'.format(ep['id']))
             episode_item = get_episode(episode['episode'], show)
             ts.append(episode_item['_timestamp'])
-            poster = '{}/static/play_button-64x64.png'.format(config.server)
+            poster = '{}/static/play_button-48x48.png'.format(config.server)
             desc = '<h5 style="margin-top:0; margin-bottom:0;"><a href="{}">{}</a></h5><small>{} &#8226; {}</small>'.format(episode_item['url'], episode_item['title'], episode_item['_display_date'], episode_item['_duration'])
-            item['content_html'] += '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div>{}</div><div style="clear:left;">&nbsp;</div></div>'.format(episode_item['_audio'], poster, desc)
+            item['content_html'] += '<div style="margin-bottom:1em;"><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div style="overflow:hidden;">{}</div><div style="clear:left;"></div></div>'.format(episode_item['_audio'], poster, desc)
+        item['content_html'] += '</blockquote>'
+        if not 'embed' in args:
+            item['content_html'] += '<div>{}</div>'.format(item['summary'].replace('\n', '<br/>'))
 
         dt = datetime.fromtimestamp(max(ts))
         item['date_published'] = dt.isoformat()
--- feedhandlers/megaphonefm.py
+++ feedhandlers/megaphonefm.py
@@ -5,55 +5,62 @@ from urllib.parse import quote_plus
 import config, utils
 
 import logging
+
 logger = logging.getLogger(__name__)
 
+
 def get_content(url, args, save_debug=False):
-  # Only handles single episodes
-  # https://playlist.megaphone.fm/?e=ESP3970143369
-  m = re.search(r'playlist\.megaphone\.fm\/\?e=(\w+)', url)
-  if not m:
-    m = re.search(r'player\.megaphone\.fm\/(\w+)', url)
+    # Only handles single episodes
+    # https://playlist.megaphone.fm/?e=ESP3970143369
+    m = re.search(r'playlist\.megaphone\.fm/\?e=(\w+)', url)
     if not m:
-      logger.warning('unable to parse episode id from ' + url)
-      return None
+        m = re.search(r'player\.megaphone\.fm/(\w+)', url)
+        if not m:
+            logger.warning('unable to parse episode id from ' + url)
+            return None
 
-  podcast_json = utils.get_url_json('https://player.megaphone.fm/playlist/episode/{}'.format(m.group(1)))
-  if not podcast_json:
-    return None
-  if save_debug:
-    utils.write_file(podcast_json, './debug/podcast.json')
+    podcast_json = utils.get_url_json('https://player.megaphone.fm/playlist/episode/{}'.format(m.group(1)))
+    if not podcast_json:
+        return None
+    if save_debug:
+        utils.write_file(podcast_json, './debug/podcast.json')
 
-  episode = podcast_json['episodes'][0]
+    episode = podcast_json['episodes'][0]
 
-  item = {}
-  item['id'] = episode['uid']
-  item['url'] = 'https://playlist.megaphone.fm/?e={}'.format(episode['uid'])
-  item['title'] = episode['title']
+    item = {}
+    item['id'] = episode['uid']
+    item['url'] = 'https://playlist.megaphone.fm/?e={}'.format(episode['uid'])
+    item['title'] = episode['title']
 
-  dt = datetime.fromisoformat(episode['pubDate'].replace('Z', '+00:00'))
-  item['date_published'] = dt.isoformat()
-  item['_timestamp'] = dt.timestamp()
-  item['_display_date'] = '{}. {}, {}'.format(dt.strftime('%b'), dt.day, dt.year)
+    dt = datetime.fromisoformat(episode['pubDate'].replace('Z', '+00:00'))
+    item['date_published'] = dt.isoformat()
+    item['_timestamp'] = dt.timestamp()
+    item['_display_date'] = '{}. {}, {}'.format(dt.strftime('%b'), dt.day, dt.year)
 
-  item['author'] = {}
-  item['author']['name'] = podcast_json['podcastTitle']
+    item['author'] = {}
+    item['author']['name'] = podcast_json['podcastTitle']
 
-  item['_image'] = episode['imageUrl']
-  item['_audio'] = episode['episodeUrlHRef']
+    item['_image'] = episode['imageUrl']
+    item['_audio'] = episode['episodeUrlHRef']
 
-  item['summary'] = episode['summary']
+    item['summary'] = episode['summary']
 
-  poster = '{}/image?height=128&url={}&overlay=audio'.format(config.server, quote_plus(item['_image']))
-  duration = []
-  t = math.floor(float(episode['duration']) / 3600)
-  if t >= 1:
-    duration.append('{} hr'.format(t))
-  t = math.ceil((float(episode['duration']) - 3600 * t) / 60)
-  if t > 0:
-    duration.append('{} min.'.format(t))
-  desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4><small>by {}<br/>{}</small>'.format(item['url'], item['title'], item['author']['name'], ', '.join(duration))
-  item['content_html'] = '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div>{}</div><div style="clear:left;"><small>{}</small></div>'.format(item['_audio'], poster, desc, item['summary'])
-  return item
+    duration = []
+    t = math.floor(float(episode['duration']) / 3600)
+    if t >= 1:
+        duration.append('{} hr'.format(t))
+    t = math.ceil((float(episode['duration']) - 3600 * t) / 60)
+    if t > 0:
+        duration.append('{} min.'.format(t))
+    item['_duration'] = ', '.join(duration)
+
+    poster = '{}/image?height=128&url={}&overlay=audio'.format(config.server, quote_plus(item['_image']))
+    desc = '<h4 style="margin-top:0; margin-bottom:0.5em;"><a href="{}">{}</a></h4><small>by {}<br/>{}</small>'.format(item['url'], item['title'], item['author']['name'], item['_duration'])
+    item['content_html'] = '<div><a href="{}"><img style="float:left; margin-right:8px;" src="{}"/></a><div style="overflow:hidden;">{}</div><div style="clear:left;"></div></div>'.format(item['_audio'], poster, desc)
+    if not 'embed' in args:
+        item['content_html'] += '<div>{}</div>'.format(item['summary'])
+    return item
+
 
 def get_feed(args, save_debug=False):
-  return None
+    return None
